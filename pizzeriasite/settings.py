2a8d338f0ed32c89f2b19957de7ee19991fdef94
Luiz Smania <luizsmania1@gmail.com>
2023-12-03 12:28:43 +0000

    set static root

--- pizzeriasite/settings.py
+++ pizzeriasite/settings.py
@@ -118,8 +118,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
-django_heroku.settings(locals())
-
 
 
 # Default primary key field type
