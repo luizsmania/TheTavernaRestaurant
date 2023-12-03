b9d6cf04dde3f59aebf177b113ca4e65cfabb667
Luiz Smania <luizsmania1@gmail.com>
2023-12-03 11:41:27 +0000

    Adding heroku requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn thetavernarestaurant.wsig:application --log-file -
--- pizzeriasite/settings.py
+++ pizzeriasite/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +15,7 @@ SECRET_KEY = 'django-insecure-edrh@_12f2n#tjohalc_z5xjhg&4z^e%4t3ce*3a7%bv&sg)d5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,7 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 
 
